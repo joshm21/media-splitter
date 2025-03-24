0bb272061be7e565234b342fecf713bc1c6b31c9
joshm21 <44173063+joshm21@users.noreply.github.com>
2025-03-24 15:22:53 +0400

    add line to show user progress

--- compress_all_video.sh
+++ compress_all_video.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Get the total number of .mp4 files
+total_files=$(ls *.mp4 | wc -l)
+file_count=0
+
 # Loop through all .mp4 files in the current directory
 for file in *.mp4; do
-    # Run ffmpeg to compress the video
-    ffmpeg -i "$file" \
+    # Increment the file count
+    file_count=$((file_count + 1))
+
+    # Echo the current file number out of the total with a color
+    echo -e "\033[1;32mProcessing file $file_count of $total_files: $file\033[0m"
+
+    # Run ffmpeg to compress the video with reduced output verbosity
+    ffmpeg -loglevel error -i "$file" \
         -vf "scale=-2:360" \
         -b:v 800k \
         -r 30 \
